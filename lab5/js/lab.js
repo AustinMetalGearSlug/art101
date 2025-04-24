d9622e63561ee3578aca3d1908dc6e72072f86d1
AustinMetalGearSlug <augallen@ucsc.edu>
2025-04-23 19:58:00 -0700

    JS code define variables and output

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -19,3 +19,22 @@ function main() {
 
 // let's get this party started
 main();
+
+//Define Variables 
+make = "Jeep";
+model = "Wrangler YJ";
+color = "Colorado Red";
+transmission = "AX 10";
+year = 1995;
+
+//calculate 
+age = 2025 - year; 
+
+//output
+document.writeln("Make: "+ make "<br>"); 
+document.writeln("Model: "+ model "<br>"); 
+document.writeln("Color: "+ color "<br>"); 
+document.writeln("Transmission: "+ transmission "<br>");
+document.writeln("Year: "+ year "<br>");
+document.writeln("Age: "+ age + "years<br>");
+
