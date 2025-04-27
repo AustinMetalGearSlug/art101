3f3126a31ab14094a96e1a3d4920300fa869b6c6
AustinMetalGearSlug <augallen@ucsc.edu>
2025-04-26 18:43:53 -0700

    Added chevy variables in JS

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Demonstrate how to use console variables 
+// Author: Austin Allen
+// Date: 04/27/2025
 
 // Constants
 
+//Define variables 
+myTransport = ("Chevy", "Jeep", "Ford");
+
 // Functions
+
+var myDreamRide = { 
+  make: "Chevy", 
+  model: "Malibu", 
+  color: "Grey", 
+  transmission: "Muncie M20", 
+  engine: "V8", 
+  year: "1973",
+  age: function() {
+  }
+}
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
