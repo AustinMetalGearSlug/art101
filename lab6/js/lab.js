184b8e8ce9e8b52abd2a5e115665e330d03eea28
AustinMetalGearSlug <augallen@ucsc.edu>
2025-04-28 11:29:51 -0700

    edited JS code

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,24 +1,10 @@
 // index.js - Demonstrate how to use and create object variables and arrays in Javascript 
 // Author: Austin Allen
-// Date: 04/27/2025
+// Date: 04/28/2025
 
-// Constants
+//Define Variables
 
-//Define variables 
-myTransport = ("Chevy", "Jeep", "Ford");
-
-// Functions
-
-var myDreamRide = { 
-  make: "Chevy", 
-  model: "Malibu", 
-  color: "Grey", 
-  transmission: "Muncie M20", 
-  engine: "V8", 
-  year: "1973",
-  age: function() {
-  }
-}
+myDreamCar = ["Chevy"];
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -27,9 +13,28 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("My Dream Ride");
+  console.log("MyDreamCar");
   // the code that makes everything happen
 }
 
 // let's get this party started
 main();
+
+//Define Variables 
+myDreamCar = { 
+  make: "Chevy", 
+  model: "Malibu", 
+  color: "Grey", 
+  transmission: "Muncie M20", 
+  engine: "V8", 
+  year: "1973",
+  age: function() {
+      return 2025 - this.year;
+  }
+}
+
+//output 
+document.writeln("Kind ofCar Want to Drive: ", myDreamCar, "</b>");
+
+document.writeln("myDreamCar: <pre>",
+  JSON.stringify (myDreamCar, null, '\t'), "</p>");
