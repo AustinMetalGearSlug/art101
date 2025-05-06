7f019c58f5b54277f11e3a8ff846c7091650d2f1
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-06 12:56:47 -0700

    js code script

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -2,6 +2,10 @@
 //Lab 8 - Anon Funcations and Callbacks 
 //Created 5/6/2025
 
+
+//Call the funcation that sorts the user's name
+let sortedName = sortUserName();
+
 function firstThing (test){
     console.log (test + "+ This is the First.<br>")
 }
