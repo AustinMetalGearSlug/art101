24029843197552f6b6040120f3089eabede8b123
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-06 14:21:36 -0700

    edited js script

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -4,9 +4,23 @@
 
 
 //Call the funcation that sorts the user's name
-let sortedName = sortUserName();
-
-function firstThing (test){
-    console.log (test + "+ This is the First.<br>")
+function isEven(x){
+    return (x % 2 == 0);
 }
 
+//test function 
+console.log ("Is 1 even?", isEven(1));
+console.log ("Is 2 even?", isEven(2));
+
+array = [2, 13, 410, 17, 5, 9, 12]
+console.log("My Array", array);
+
+var result = array.map(isEven);
+//should return [true, false, true, false, false]
+console.log("Test of eveness of array:" , result); 
+
+var result = array.map(function(x){
+    return x ** 0.5;
+})
+
+console.log("Sqaureroot of array:", result);
