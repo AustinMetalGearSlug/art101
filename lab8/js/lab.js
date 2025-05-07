07f092c988da96621360108d004788b54f734629
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-06 21:51:43 -0700

    edit lab

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -14,24 +14,23 @@ console.log ("Is 2 even?", isEven(2));
 array = [2, 13, 410, 17, 5, 9, 12]
 console.log("My Array", array);
 
-var result = array.map(isEven);
+var result1 = array.map(isEven);
 //should return [true, false, true, false, false]
-console.log("Test of eveness of array:" , result); 
+console.log("Test of Eveness of Array:" , result1); 
 
 
-var result = array.map(function(x){
+var result2 = array.map(function(x){
     return x ** 0.5;
 })
 
-console.log("Sqaureroot of array:", result);
+console.log("Sqaureroot of Array:", result2);
 
   // Your map results data
 var mapResults = "Your map results here";
 
- // Use jQuery to select the element by its ID and set the HTML content
- $("#output").html(mapResults);
- 
- 
 // Use jQuery to select the element by its ID and set the HTML content
-let $final = ("output").html(mapResult); 
+var mapResult = "<b>Array Result:</b> " + array +
+    "<br><b>Test of Eveness of Array:</b> " + result1 +
+    "<br><b>Square Root of Array:</b> " + result2;
 
+$("#output").html(mapResult);
