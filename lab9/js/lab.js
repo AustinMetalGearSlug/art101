11db71af5f158cbc4782bc5eccb53f8f1beb0169
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-08 08:43:43 -0700

    added HTML body and JS code

---
+++ lab9/js/lab.js
@@ -0,0 +1,13 @@
+
+
+let aColors=["purple", "red", "green", "yellow"];
+
+function fDoColors () { 
+
+    //$ means jqueary, "()" mean a function, # stands for CSS, used to choose the id, cuh as id=output
+    // thing # as "id = "
+
+    $("#output").css("background",color);
+
+}
+
