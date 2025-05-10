1b82d607efb23d33f1018465be3eedb6aa829e9a
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-09 22:17:23 -0700

    added JS code

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,9 +3,17 @@
 
    Requirements: jQuery must be loaded for this script to work.
 
-   Author: Austin Allrn (Maverick)
+   Author: Austin Allen (Maverick)
    Date: 05/08/2025 
 */
 
-//add button to challenege 
+//add button to "What ever you do...don't pust this button or you'll become invsibible"
+$('#challenge').append("<button id='button-challenge'>Da Button</button>");
+
+//add a click listener to the invisible button 
+$(".button-challnge").click(function){
+ // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+}
+
 
