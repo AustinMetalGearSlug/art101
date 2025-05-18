6a78a81c1fd94385e445eb00e2a60ab9f64eaa4e
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-17 23:49:04 -0700

    created CSS, JS and HTML code for JS

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -6,19 +6,13 @@
 */
 
 // Sorts the characters of a string in alphabetical order.
-function sortString(inputString) {
-    // We have to convert our string to an array and back again to sort it
-  return inputString.split('').sort().join('');
-}
+$("#action").click(
 
-// click listener for button
-$("#submit").click(function(){
-});
+  function(){
+    let userText=$("#textbox").val();
+   $("#output").append('<div class="text"><p>' + userText + '</p></div>');
+    /// our action 
+    
+  }
 
-// get value of input field
-const userName = $("#user-name").val();
-
-// now let's sort it
-userNameSorted = sortString(userName);
-// append a new div to our output div
-$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+);
