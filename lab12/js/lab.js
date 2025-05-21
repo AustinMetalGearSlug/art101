48dc76d477c61956f00637d6655c6b9a97b1909d
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-21 13:33:37 -0700

    added text lines

---
+++ lab12/js/lab.js
@@ -0,0 +1,20 @@
+/*
+lab 12 - Using conditionals that will select an output based on a user's input based on the length of their name.
+   Requirements: Create a function that depends on conditionals.
+   Author: Austin Allen 
+   Date: 05/17/2025 
+*/
+
+shipArray =[
+{
+    title: "USS Enterprise - NCC-1701-D",
+    text: "You will be assigned to the star ship, USS Enterprise NCC-1701-D. Galaxy class is a exploratory and science vessel used for exploration and contact with new life and going where no man has gone before.",
+},
+{
+    title: "IKS - Rotarran",
+    text: "You have the honor of serving aboard the Klingon Bird of Prey vessel, the IKS Rottarran. Using stealth and striking tactics you will be bring honor and glory to you and your house, Qapla!!!",
+}, 
+   title: "UNSC - Spirit of Fire",
+   text: 
+]
+
