3730b38ff7d7934e8adc88ec54e0866a2b82569c
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-22 10:37:11 -0700

    added JS code

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -5,4 +5,42 @@ lab 12 - Using conditionals that will select an output based on a user's input b
    Date: 05/17/2025 
 */
 
-let variants = ["Spinosaurus", "Acrocanthosaurus", "Utahraptor", "Tyrannosaurus Rex", "Giganotosaurus", "Triceratops", "Ankylosaurus"]
+let variants = ["Spinosaurus", "Acrocanthosaurus", "Utahraptor", "Tyrannosaurus Rex", 
+    "Giganotosaurus", "Triceratops", "Ankylosaurus", "Iguanodon"]
+
+    function sortIntoDino( dataLength ) {
+  
+  let remainer=dataLength%4;  
+  $("#output").append(".*RAWR*... You are a " + variants[remainer]);
+  
+}
+
+function whatHappensOnClick(){
+  
+    console.log("click");
+    
+    let data=$("#input").val();
+  
+    let dataLength=data.length;
+  
+    if ( data && dataLength<=12 ) {
+      
+      $("#output").append(" " +  data);
+      
+      sortIntoDino(dataLength);
+      
+      console.log("there is some data");
+      
+    }
+  
+    else if (dataLength>=12){
+     
+      $("#output").append("Too much of a name");
+    }
+    else {
+      
+      $("#output").append("Please enter a name");  
+    }
+}
+
+$("#button").click( whatHappensOnClick );
