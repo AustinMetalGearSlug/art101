1064b19e841dd1b7d5cecb2c25dc6f8b1699020a
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-22 09:14:41 -0700

    deleted array and changing to dinoarray

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -5,27 +5,6 @@ lab 12 - Using conditionals that will select an output based on a user's input b
    Date: 05/17/2025 
 */
 
-shipArray =[
-{
-    title: "USS Defiant - NCC 74205",
-    text: "You will be assigned to the star ship, USS Defiant NCC-74205. Escort type class is the first line of defense for the Federation of Planets. Figting aganist Dominion and Borg threats.",
-},
-{
-    title: "IKS - Rotarran",
-    text: "You have the honor of serving aboard the Klingon Bird of Prey vessel, the IKS Rottarran. Using stealth and striking tactics you will be bring honor and glory to you and your house, Qapla!!!",
-},
-{
-   title: "UNSC - Spirit of Fire",
-   text: "You are assigned to the UNSC vessel, the Spirit of Fire, a Phoenix-class colonial support vessel, though outdated it is maned by combat veterans and an expirenced crew ready for any war...a Halo Wars.",
-},
-{
-   title: "Class 6 Super Destroyer", 
-   text: "For liberty and managed democracy who are a helldiver an assigned to a Class 6 interplanetary Combat Vessel, Super Destroyer. Be the sword of freedom and bring libetry to vanquish automation and alien threats.",
-},
-{
- title: "",
- text:""
-},
+dinoArray =[
 
 ]
-
