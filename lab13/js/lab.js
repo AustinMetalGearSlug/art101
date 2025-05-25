203fce99d08e0cdcad9cf668e01d592b6d7bc1c9
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-24 18:25:13 -0700

    added code for button

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,30 +1,35 @@
 /*
 lab 13 - Using loops to print "Race Car" for multiplies of 2, 6, and 12 instead of a number
-   Requirements: Create a loop function that prints "Race Car" instead of number and prints from 1 to 100
+   Requirements: Create a loop function that prints "Pizza Da Hutt" instead of number and prints from 1 to 100
    Author: Austin Allen 
    Date: 05/24/2025 
 */
+function whatHappensOnClick() {
+    console.log("click");
 
-// Use a for loop to print all the numbers from 1 to 100. Test and debug.
-for (let i = 1; i <= 100; i++) {
-  // For numbers which are multiples of both 2 and 6, print “Race Car” instead.
-  if(i % 12 == 0) {
-    console.log("Race Car!");
-    
-  }
-  // Add an if statement: if the number is a multiple of 2, print “Race” instead of the number.
-  else if (i % 2 == 0) {
-    console.log("Car!");
-  }
-  // Add an else if condition: If the number is a multiple of 6, print “Car” instead of the number.
-  else if(i % 6 == 0) {
-    console.log("Race!");
-    
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = ""; 
+
+    for (let i = 1; i <= 30; i++) {
+      let message = "";
+
+      if (i % 6 === 0 && i % 2 === 0) {
+        message = "Pizza Da Hutt!";
+      } else if (i % 4 === 0) {
+        message = "Hutt!";
+      } else if (i % 2 === 0) {
+        message = "Pizza!";
+      } else {
+        message = i;
+      }
+
+      const p = document.createElement("p");
+      p.textContent = message;
+      outputDiv.appendChild(p);
+    }
   }
 
-  else {
-    console.log(i);
-  }
-  
-}
-const outputDiv = document.getElementById("output");
+  // Attach the click event using jQuery
+  $(document).ready(function () {
+    $("#button").click(whatHappensOnClick);
+  });
