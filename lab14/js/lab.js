2fc18eab7080277fdb0912adc518d12213cb2b49
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-27 15:00:51 -0700

    created button

---
+++ lab14/js/lab.js
@@ -0,0 +1,9 @@
+/*
+lab 14 - Debug and refactor a previous lab
+   Requirements: Refactor and debug a preivous lab by inspecting any issues from the console or to fully complete a previous lab
+   Author: Austin Allen 
+   Date: 05/27/2025 
+*/
+document.getElementById("button").addEventListener("click", function() {
+        window.location.href = "lab14_factoring.html";
+    });
