35b7641209c2aeff164cdb0eecbebaba3ecc98f7
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-29 08:59:04 -0700

    redoing lab title

---
+++ lab14_factoring/js/lab.js
@@ -0,0 +1,7 @@
+/*
+lab 14 - Debug and refactor a previous lab
+   Requirements: Refactor and debug a preivous lab by inspecting any issues from the console or to fully complete a previous lab
+   Author: Austin Allen 
+   Date: 05/27/2025 
+*/
+
