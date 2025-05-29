78ee5869bfca7f715f043d5f29660657f70e459b
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-29 08:48:42 -0700

    created api ref

--- experiments/lab.js
+++ experiments/lab.js
@@ -1,35 +1,27 @@
-var index=1;
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://yesno.wtf/api",
+    // The data to send (will be converted to a query string)
+    data: { 
+           
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+        $("#output").append(data.answer);
+        $("#output").append("<img src="+data.image+">");
 
-let aColors=["purple", "red", "green", "yellow"];
-
-function fDoColors(color){ 
-
-    //$ means jqueary, "()" mean a function, # stands for CSS, used to choose the id, cuh as id=output
-    // thing # as "id = "
-    // "color" is a virable name 
-
- $("#output").css("background",color);
-   console.log(color);
-
-   let newColor=color+aColors[index];
-
- //"=", defining the variable
- index=index+1;
-
-   console.log(newColor);
-
-}
-
-//fDoColors("yellow");
-
-aColors.map(fDoColors); 
-
-//Generate text box
-$("#action").click(
-
- function(){
-    let
-userText=$("#textbox").val();
-  $("#output").append( "<p class=message>"+ userText );
-
- });
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) { 
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+})
