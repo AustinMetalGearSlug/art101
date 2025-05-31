d4e514af14e0af3dc826461b5e6862a729f45cb2
AustinMetalGearSlug <augallen@ucsc.edu>
2025-05-30 22:54:34 -0700

    added api

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -6,40 +6,18 @@ lab 15 - AJAX
 */
 
 //use AJAX to connect to yesno.wtf/api
+ // When a user clicks the button
+    $("button").click(function () {
+      console.log("Button clicked!");
 
-// Go to https://yesno.wtf/api
-
-const ajaxObj = {
-  url: "https://yesno.wtf/api",
-  // data: {},
-  type: "GET",
-  dataType: "json"
-}
-
-// When a user clicks the button
-$("#sumbuton").click(function() {
-  console.log("click!");
-  // use a jQuery AJAX call to fetch output
-  $.ajax(ajaxObj)
-  // callback for success - chained to xjax
-  .done(function(data){
-    console.log("Success!");
-    console.log(data);
-    // extract the ansewr from data
-    let answer = data.answer;
-    // extract image from data
-    let imageURL = data.image;
-    // build image html
-    //let imageHTML = "<img src='" + imageURL + "'>";
-    // equivalent to above - but using string interpolation
-    let imageHTML = `<img src='${imageURL}'>`;
-    // Insert the output in the output div
-    $("#output").html("<h1>" + answer);
-    $("#output").append(imageHTML);
-  })
-  // callback for failure - chained to ajax
-  .fail(function( xhr, status, errorThrown ) { 
-    console.log(errorThrown + " Status:" + status );
-  });
-})
-
+      // Send a GET request using jQuery to the Dino Ipsum API
+      $.get('https://dinoipsum.com/api/?format=html&words=10&paragraphs=2')
+        .done(function (html) {
+          console.log("Wheres the Dinos!");
+         // Display the HTML response
+          $('#some-awesome-container').html(html);
+        })
+        .fail(function (xhr, status, errorThrown) {
+          console.log("Error: " + errorThrown + " Status: " + status);
+        });
+    });
