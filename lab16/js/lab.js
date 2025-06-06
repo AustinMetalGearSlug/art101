3a8435f98ffb875446751325f16358d3834238c4
AustinMetalGearSlug <augallen@ucsc.edu>
2025-06-05 20:10:48 -0700

    button output

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,77 +5,41 @@ lab 16 - JSON and APIs
    Date: 06/05/2025 
 */
 
-// ajax object
-const ajaxObject = {
-  url: "https://xkcd.com/info.0.json",
-  data: {},
-  type: "GET",
-  dataType: "json"
-};
-// initialize variable
-var maxNum;
-var newURL;
+$("#getComic").click(function () {
+  // initialize variable
+  var maxNum;
+  var newURL;
 
-// Using the core $.ajax() method
-$.ajax({
-    // The URL for the request (from the api docs)
+  // First AJAX call to get the latest comic number
+  $.ajax({
     url: "https://corsproxy.io/?https://xkcd.com/info.0.json",
-    // The data to send (will be converted to a query string)
-    data: {
-        // here is where any data required by the api 
-        //   goes (check the api docs)
-        // id: 123,
-        // api_key: "blahblahblah",
-    },
-    // Whether this is a POST or GET request
     type: "GET",
-    // The type of data we expect back
     dataType: "json",
-    // What do we do when the api call is successful
-    //   all the action goes in here
     success: function (data) {
-        // do stuff
-        //console.log(data);
-        maxNum = data.num;
-        //console.log(maxNum);
-        newURL = "https://corsproxy.io/?https://xkcd.com/" + Math.floor(Math.random() * maxNum) + "/info.0.json";
+      maxNum = data.num;
+      // Generate random comic URL
+      newURL = "https://corsproxy.io/?https://xkcd.com/" + Math.floor(Math.random() * maxNum) + "/info.0.json";
 
-        $.ajax({
-            // The URL for the request (from the api docs)
-            url: newURL,
-            // The data to send (will be converted to a query string)
-            data: {
-                // here is where any data required by the api 
-                //   goes (check the api docs)
-                // id: 123,
-                // api_key: "blahblahblah",
-            },
-            // Whether this is a POST or GET request
-            type: "GET",
-            // The type of data we expect back
-            dataType: "json",
-            // What do we do when the api call is successful
-            //   all the action goes in here
-            success: function (data) {
-                // do stuff
-                //console.log(data);
-                var titleText = data.title;
-                var imageURL = data.img;
-                var imageAlt = data.alt;
-                $("#output").html("<h3>" + titleText + "</h3>");
-                $("#output").append("<img src='" + imageURL + "'>");
-                $("#output").append("<p>" + imageAlt + "</p>");
-            },
-            // What we do if the api call fails
-            error: function (jqXHR, textStatus, errorThrown) {
-                // do stuff
-                console.log("Error:", textStatus, errorThrown);
-            }
-        })
+      // Second AJAX call for random comic
+      $.ajax({
+        url: newURL,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+          var titleText = data.title;
+          var imageURL = data.img;
+          var imageAlt = data.alt;
+          $("#output").html("<h3>" + titleText + "</h3>");
+          $("#output").append("<img src='" + imageURL + "' alt='" + imageAlt + "'>");
+          $("#output").append("<p>" + imageAlt + "</p>");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log("Error loading random comic:", textStatus, errorThrown);
+        }
+      });
     },
-    // What we do if the api call fails
     error: function (jqXHR, textStatus, errorThrown) {
-        // do stuff
-        console.log("Error:", textStatus, errorThrown);
+      console.log("Error getting latest comic number:", textStatus, errorThrown);
     }
-})
+  });
+});
